facf08860328f9df7328eaa9548f3ad237ff8764
Songkran-060 <Pongpichet.sk@mail.kmutt.ac.th>
2022-05-23 15:49:20 +0700

    NavBar UI

---
+++ src/components/NavBar.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="navbar">
+    <div class="bg-gradient-to-r from-purple-900 to-purple-700 py-1.5 px-44 flex">
+      <div class="flex">
+        <span class="text-white font-bold align-middle">Eventmod</span>
+        <img src="../assets/logo.png" class="ml-1" />
+      </div>
+      <div class=" ml-auto">
+        <span class="text-white">New Activity</span>
+        <span class="text-white bg-gradient-to-r from-orange-500 to-orange-300 rounded-full px-2 ml-3">Username</span>
+      </div>
+    </div>
+	</div>
+
+</template>
+
+<script>
+
+export default {
+	components: {
+
+		},
+	props: {
+
+	},
+	emits: [
+	],
+	data() {
+		return {
+
+		}
+	},
+	methods: {
+
+	},
+	async created() {
+
+	}
+};
+</script>
