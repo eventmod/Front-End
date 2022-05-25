5940757f68abf3a8e43883decb57af4b515f86fa
Songkran-060 <Pongpichet.sk@mail.kmutt.ac.th>
2022-05-25 15:45:37 +0700

    UI NavBar & Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="footer">
+    <div class="grid grid-cols-6">
+      <span class="bg-violet-500 text-xs h-3">&nbsp;</span>
+      <span class="bg-violet-800"></span>
+      <span class="bg-violet-100"></span>
+      <span class="bg-orange-300"></span>
+      <span class="bg-orange-400"></span>
+      <span class="bg-red-400"></span>
+    </div>
+    <div class="h-52 py-16">
+      <div class="flex flex-row mx-auto gap-x-2 w-fit">
+        <span class="font-medium text-lg select-none">Eventmod</span>
+        <img src="../assets/logo.png" class="" />
+      </div>
+      <div class="text-center w-full mt-8">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      </div>
+    </div>
+	</div>
+</template>
+
+<script>
+
+export default {
+	components: {
+
+	},
+	props: {
+
+	},
+	emits: [
+	],
+	data() {
+		return {
+
+		}
+	},
+	methods: {
+
+	},
+	async created() {
+
+	}
+};
+</script>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,17 +1,16 @@
 <template>
 	<div class="navbar">
     <div class="bg-gradient-to-r from-purple-900 to-purple-700 py-1.5 px-44 flex">
-      <div class="flex">
-        <span class="text-white font-bold align-middle">Eventmod</span>
+      <div class="flex flex-row">
+        <span class="text-white font-bold align-middle select-none">Eventmod</span>
         <img src="../assets/logo.png" class="ml-1" />
       </div>
       <div class=" ml-auto">
-        <span class="text-white">New Activity</span>
-        <span class="text-white bg-gradient-to-r from-orange-500 to-orange-300 rounded-full px-2 ml-3">Username</span>
+        <span class="text-white select-none">New Activity</span>
+        <span class="text-white bg-gradient-to-r from-orange-500 to-orange-300 select-none rounded-full px-2 ml-3 align-middle">Username</span>
       </div>
     </div>
 	</div>
-
 </template>
 
 <script>
@@ -19,7 +18,7 @@
 export default {
 	components: {
 
-		},
+	},
 	props: {
 
 	},
