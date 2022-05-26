d3a8da67a71895c3f6ed24491aa211c5092dd1fd
Songkran-060 <Pongpichet.sk@mail.kmutt.ac.th>
2022-05-26 18:04:57 +0700

    [EDIT] NavBar Component & Global Registration Component

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,12 +1,12 @@
 <template>
 	<div class="navbar">
     <div class="bg-gradient-to-r from-purple-900 to-purple-700 py-1.5 px-44 flex">
-      <div class="flex flex-row">
+      <a href="/" class="flex flex-row">
         <span class="text-white font-bold align-middle select-none">Eventmod</span>
         <img src="../assets/logo.png" class="ml-1" />
-      </div>
+      </a>
       <div class=" ml-auto">
-        <span class="text-white select-none">New Activity</span>
+        <a href="/create" class="text-white select-none">New Activity</a>
         <span class="text-white bg-gradient-to-r from-orange-500 to-orange-300 select-none rounded-full px-2 ml-3 align-middle">Username</span>
       </div>
     </div>
