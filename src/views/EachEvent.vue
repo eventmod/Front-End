952b3129708f3d954bac2c22547dee60e0afadae
Songkran-060 <Pongpichet.sk@mail.kmutt.ac.th>
2022-08-03 12:14:48 +0700

    [EDIT] Event Detail

--- src/views/EachEvent.vue
+++ src/views/EachEvent.vue
@@ -2,18 +2,33 @@
   <div id="aa" class="Each Event pt-52">
     <NavBar class="overflow-hidden fixed top-0 w-full z-10" />
     <div class="flex flex-col bg-white mx-80 z-20 rounded-lg shadow-lg px-10 py-12 gap-y-8">
-      <span class="font-bold text-4xl">{{ event.eventName }}</span>
+      <span class="font-bold text-4xl">{{ event.eventTitle }}</span>
       <!-- <span class="font-normal text-sm text-gray-400 -mt-4">3 May 2022</span> -->
       <img :src="`${host}/Files/${event.eventCover}`" class="w-full rounded-lg mx-auto object-cover" />
       <div class="text-justify">
-        {{ event.eventDescription }}
+        {{ event.eventLongDescription }}
       </div>
       <!-- Date & Time (Start event - end event) -->
       <div class="flex flex-col">
         <span class="font-bold">Date & Time (Start event - end event)</span>
-        <div class="flex flex-row gap-x-4">
-          <span class="">Tuesday, May 3</span>
-          <span class="">9:30 am - 12:00 pm</span>
+        <div class="flex flex-row">
+          <span class="">{{ day(new Date(event.eventStartDate)) }},&nbsp;</span>
+          <span class="">{{ month(new Date(event.eventStartDate)) }}&nbsp;</span>
+          <span class="">{{ date(new Date(event.eventStartDate)) }}</span>
+          <span class="">&nbsp;-&nbsp;</span>
+          <span class="">{{ day(new Date(event.eventEndDate)) }},&nbsp;</span>
+          <span class="">{{ month(new Date(event.eventEndDate)) }}&nbsp;</span>
+          <span class="">{{ date(new Date(event.eventEndDate)) }}</span>
+          <span class="">&nbsp;|&nbsp;</span>
+          <span class="">{{ hours(new Date(event.eventStartDate + " " + event.eventStartTime)) }}</span>
+          <span class="">:</span>
+          <span class="">{{ minutes(new Date(event.eventStartDate + " " + event.eventStartTime)) }}&nbsp;</span>
+          <span class="">{{ ampm(new Date(event.eventStartDate + " " + event.eventStartTime)) }}</span>
+          <span class="">&nbsp;-&nbsp;</span>
+          <span class="">{{ hours(new Date(event.eventEndDate + " " + event.eventEndTime)) }}</span>
+          <span class="">:</span>
+          <span class="">{{ minutes(new Date(event.eventEndDate + " " + event.eventEndTime)) }}&nbsp;</span>
+          <span class="">{{ ampm(new Date(event.eventEndDate + " " + event.eventEndTime)) }}</span>
         </div>
       </div>
       <!-- Date & Time (Start event - end event) -->
@@ -41,21 +56,22 @@
         </div>
         <!-- Number of Participant -->
         
-        <!-- Gender -->
-        <div class="grid grid-cols-3">
-          <span class="font-bold">Gender</span>
-          <span class="">{{ event.eventGender }}</span>
-        </div>
-        <!-- Gender -->
       </div>
 
       <div class="grid grid-cols-2 gap-y-8">
         <!-- First day for Recruitment -->
         <div class="flex flex-col">
           <span class="font-bold">First day for Recruitment</span>
-          <div class="flex flex-row gap-x-4">
-            <span class="">{{ this.eventFirstRegis.day }}, {{ this.eventFirstRegis.month }} {{this.eventFirstRegis.date}} {{ this.eventFirstRegis.year }}</span>
-            <span class="">{{ this.eventFirstRegis.hour }}:{{ this.eventFirstRegis.minute }} {{ this.eventFirstRegis.ampm }}</span>
+          <div class="flex flex-row">
+            <span class="">{{ day(new Date(event.eventStartRegis)) }},&nbsp;</span>
+            <span class="">{{ month(new Date(event.eventStartRegis)) }}&nbsp;</span>
+            <span class="">{{ date(new Date(event.eventStartRegis)) }}&nbsp;</span>
+            <span class="">{{ year(new Date(event.eventStartRegis)) }}&nbsp;</span>
+            <span class="">|&nbsp;</span>
+            <span class="">{{ hours(new Date(event.eventStartRegis)) }}</span>
+            <span class="">:</span>
+            <span class="">{{ minutes(new Date(event.eventStartRegis)) }}&nbsp;</span>
+            <span class="">{{ ampm(new Date(event.eventStartRegis)) }}</span>
           </div>
         </div>
         <!-- First day for Recruitment -->
@@ -63,9 +79,16 @@
         <!-- Last day for Recruitment -->
         <div class="flex flex-col">
           <span class="font-bold">Last day for Recruitment</span>
-          <div class="flex flex-row gap-x-4">
-            <span class="">{{ this.eventLastRegis.day }}, {{ this.eventLastRegis.month }} {{this.eventLastRegis.date}} {{ this.eventLastRegis.year }}</span>
-            <span class="">{{ this.eventLastRegis.hour }}:{{ this.eventLastRegis.minute }} {{ this.eventLastRegis.ampm }}</span>
+          <div class="flex flex-row">
+            <span class="">{{ day(new Date(event.eventEndRegis)) }},&nbsp;</span>
+            <span class="">{{ month(new Date(event.eventEndRegis)) }}&nbsp;</span>
+            <span class="">{{ date(new Date(event.eventEndRegis)) }}&nbsp;</span>
+            <span class="">{{ year(new Date(event.eventEndRegis)) }}&nbsp;</span>
+            <span class="">|&nbsp;</span>
+            <span class="">{{ hours(new Date(event.eventEndRegis)) }}</span>
+            <span class="">:</span>
+            <span class="">{{ minutes(new Date(event.eventEndRegis)) }}&nbsp;</span>
+            <span class="">{{ ampm(new Date(event.eventEndRegis)) }}</span>
           </div>
         </div>
         <!-- Last day for Recruitment -->
@@ -110,24 +133,6 @@ export default {
 	data() {
 		return {
       event: [],
-      eventFirstRegis: {
-        year: '',
-        day: '',
-        month: '',
-        date: '',
-        hour: '',
-        minute: '',
-        ampm: ''
-      },
-      eventLastRegis: {
-        year: '',
-        day: '',
-        month: '',
-        date: '',
-        hour: '',
-        minute: '',
-        ampm: ''
-      },
 
       host: process.env.VUE_APP_EVENTMOD_HOST,
 		}
@@ -141,45 +146,43 @@ export default {
       return data;
     },
 
-    async splitDate() {
+    day(x) {
+      const dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+      return dayOfWeek[x.getDay()];
+    },
 
-      var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    month(x) {
       const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      return monthNames[x.getMonth()];
+    },
 
-      const firstRegis = new Date(this.event.eventStartRegis)
-      const lastRegis = new Date(this.event.eventEndRegis)
+    year(x) {
+      return x.getFullYear();
+    },
 
-      this.eventFirstRegis.year = firstRegis.getFullYear()
-      this.eventFirstRegis.day = dayOfWeek[firstRegis.getDay()]
-      this.eventFirstRegis.month = monthNames[firstRegis.getMonth()]
-      this.eventFirstRegis.date = firstRegis.getDate()
-      this.eventFirstRegis.hour = firstRegis.getHours()
-      this.eventFirstRegis.minute = firstRegis.getMinutes()
+    date(x) {
+      return x.getDate()
+    },
 
-      this.eventFirstRegis.ampm = this.eventFirstRegis.hour >= 12 ? 'pm' : 'am';
-      this.eventFirstRegis.hour = this.eventFirstRegis.hour % 12;
-      this.eventFirstRegis.hour = this.eventFirstRegis.hour ? this.eventFirstRegis.hour : 12;
-      this.eventFirstRegis.hour = this.eventFirstRegis.hour <=9 ? '0' + this.eventFirstRegis.hour : this.eventFirstRegis.hour;
-      this.eventFirstRegis.minute = this.eventFirstRegis.minute <=9 ? '0' + this.eventFirstRegis.minute : this.eventFirstRegis.minute;
+    hours(x) {
+      var hour = x.getHours()
+      hour = hour <=9 ? '0' + hour : hour;
+      return hour
+    },
 
-      this.eventLastRegis.year = lastRegis.getFullYear()
-      this.eventLastRegis.day = dayOfWeek[lastRegis.getDay()]
-      this.eventLastRegis.month = monthNames[lastRegis.getMonth()]
-      this.eventLastRegis.date = lastRegis.getDate()
-      this.eventLastRegis.hour = lastRegis.getHours()
-      this.eventLastRegis.minute = lastRegis.getMinutes()
+    minutes(x) {
+      var minute = x.getMinutes()
+      minute = minute <=9 ? '0' + minute : minute;
+      return minute
+    },
 
-      this.eventLastRegis.ampm = this.eventLastRegis.hour >= 12 ? 'pm' : 'am';
-      this.eventLastRegis.hour = this.eventLastRegis.hour % 12;
-      this.eventLastRegis.hour = this.eventLastRegis.hour ? this.eventLastRegis.hour : 12;
-      this.eventLastRegis.hour = this.eventLastRegis.hour <=9 ? '0' + this.eventLastRegis.hour : this.eventLastRegis.hour;
-      this.eventLastRegis.minute = this.eventLastRegis.minute <=9 ? '0' + this.eventLastRegis.minute : this.eventLastRegis.minute;
-
-    }
+    ampm(x) {
+      var ampm = x.getHours() >= 12 ? 'pm' : 'am';
+      return ampm
+    },
 	},
 	async created() {
     this.event = await this.fetchEvent();
-    this.splitDate();
 	}
 }
 </script>
