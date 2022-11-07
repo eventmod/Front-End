8785b76d729ae40201bd3ece015e408cd5a879b5
Songkran-060 <Pongpichet.sk@mail.kmutt.ac.th>
2022-11-07 12:26:15 +0700

    [LIFF] Authen add 2 label fields

--- src/views/LIFFAuthenKMUTT.vue
+++ src/views/LIFFAuthenKMUTT.vue
@@ -1,9 +1,17 @@
 <template>
-  <div class="bg-white flex flex-col mx-auto w-3/4 space-y-8">
-    <input id="inputStudentId" type="text" v-model="inputStudentId" placeholder="Student Id 11 digits..." minlength="11" maxlength="11" 
-      class="rounded-md focus:outline-none h-12 py-1 px-2 shadow-md bg-gray-100" />
-    <input id="inputStudentMail" type="text" v-model="inputStudentMail" placeholder="@mail.kmutt.ac.th / @kmutt.ac.th" minlength="11" maxlength="11" 
-      class="rounded-md focus:outline-none h-12 py-1 px-2 shadow-md bg-gray-100" />
+  <div class="h-screen flex flex-col space-y-8 justify-center place-items-center">
+    <!-- <div class="h-screen"> -->
+      <div class="w-3/4">
+        <label for="inputStudentId" class="">KMUTT Student ID</label>
+        <input id="inputStudentId" type="text" v-model="inputStudentId" placeholder="Student Id 11 digits..." minlength="11" maxlength="11" 
+          class="rounded-md focus:outline-none h-12 py-1 px-2 shadow-md bg-gray-100 w-full" />
+      </div>
+      <div class="w-3/4">
+        <label for="inputStudentMail" class="">KMUTT Student Mail</label>
+        <input id="inputStudentMail" type="text" v-model="inputStudentMail" placeholder="@mail.kmutt.ac.th / @kmutt.ac.th"
+          class="rounded-md focus:outline-none h-12 py-1 px-2 shadow-md bg-gray-100 w-full" />
+      </div>
+    <!-- </div> -->
   </div>
 </template>
 
