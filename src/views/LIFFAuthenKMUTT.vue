ec2b762043a1f1122c2605eda069386e7d10617e
Songkran-060 <Pongpichet.sk@mail.kmutt.ac.th>
2022-11-07 10:24:34 +0700

    [LIFF] Authen add 2 input fields

--- src/views/LIFFAuthenKMUTT.vue
+++ src/views/LIFFAuthenKMUTT.vue
@@ -1,15 +1,10 @@
 <template>
-  <div>
-    <h1>create-liff-app</h1>
-    <p v-if="param">{{ param }}</p>
-    <p v-if="error">
-      {{ error }}
-    </p>
-    <a href="https://developers.line.biz/ja/docs/liff/" target="_blank" rel="noreferrer">
-      LIFF Documentation
-    </a>
+  <div class="bg-white flex flex-col mx-auto w-3/4 space-y-8">
+    <input id="inputStudentId" type="text" v-model="inputStudentId" placeholder="Student Id 11 digits..." minlength="11" maxlength="11" 
+      class="rounded-md focus:outline-none h-12 py-1 px-2 shadow-md bg-gray-100" />
+    <input id="inputStudentMail" type="text" v-model="inputStudentMail" placeholder="@mail.kmutt.ac.th / @kmutt.ac.th" minlength="11" maxlength="11" 
+      class="rounded-md focus:outline-none h-12 py-1 px-2 shadow-md bg-gray-100" />
   </div>
-  <!-- <div>Hello World</div> -->
 </template>
 
 <script>
@@ -18,8 +13,9 @@ import liff from "@line/liff";
 export default {
   data() {
     return {
-      param: "",
-      error: ""
+      userId: "",
+      inputStudentId: "",
+      inputStudentMail: "",
     };
   },
   methods: {
@@ -27,10 +23,10 @@ export default {
   },
   async created() {
     await liff.init({ liffId: '1657624777-GQkya04l' });
-    if (!liff.isLoggedIn()) {
-      liff.login()
-    }
-    this.param = (await liff.getProfile()).userId
+    // if (!liff.isLoggedIn()) {
+    //   liff.login()
+    // }
+    this.userId = (await liff.getProfile()).userId
   },
 };
 </script>
